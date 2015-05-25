9bdc4a983d331a3579d624d71f6498c751934b18
eee88836 <yangchengyi821113@gaml.com>
2015-05-25 15:43:09 +0800

    update 6

--- ObserverSample/src/tw/edu/npu/mis/ObserverSample.java
+++ ObserverSample/src/tw/edu/npu/mis/ObserverSample.java
@@ -36,16 +36,25 @@ import java.util.List;
 public class ObserverSample {
 
     public static void main(String[] args) {
-        // Initialize MVC and Window objects.
+      
         Window window = new Window();
         Model model = new Model();
+        List<Observer> mInvalidViews = new ArrayList<>();
+        
         Controller controller = new Controller(model);
-        List<View> views = new ArrayList<>();
-        views.add(new View("View 1", window, model));
-        views.add(new View("View 2", window, model));
-        views.add(new View("View 3", window, model));
+        
+        mInvalidViews.add(new AlternativeView("View ", window, model));
+        
+        while (true) {
+            controller.readInput();
+            
+            for (Observer vv : mInvalidViews) {
+                vv.update();
+            }
+            
+        }
 
-        // Start the event loop.
-        window.startEventLoop(controller, views);
+        
+
     }
 }
